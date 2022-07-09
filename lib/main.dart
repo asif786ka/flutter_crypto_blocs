7ccd29b41bf1832510816ce8e3c188dd6f721519
asifmacmini <asif@thinkific.com>
2022-07-09 09:53:50 +0530

    Added bloc and connected to ui.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:cryptobloc/repositories/crypto_repository.dart';
 import 'package:cryptobloc/screens/home_screen.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'blocs/crypto/crypto_bloc.dart';
 
 void main() {
   EquatableConfig.stringify = kDebugMode;
@@ -11,14 +15,24 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Crypto Bloc App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.black,
-        accentColor: Colors.tealAccent,
+    return RepositoryProvider(
+      create: (context) => CryptoRepository(),
+      child: MaterialApp(
+        title: 'Flutter Crypto Bloc App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.black,
+          accentColor: Colors.tealAccent,
+        ),
+        home: BlocProvider(
+          create: (context) =>
+          CryptoBloc(
+            cryptoRepository: context.read<CryptoRepository>(),
+          )
+            ..add(AppStarted()),
+          child: HomeScreen(),
+        ),
       ),
-      home: HomeScreen(),
     );
   }
 }
